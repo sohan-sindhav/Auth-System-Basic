5df69736ff95adff69296c3ce1dd6add85cd4690
sohan sindhav <sohann7704@gmail.com>
2026-03-10 23:16:31 +0530

    frontend vercel url updated to backend

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://auth-system-basic-32cy.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
