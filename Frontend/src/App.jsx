5dff07675535731049502a35e9cd24dd9e092399
sohan sindhav <sohann7704@gmail.com>
2026-03-10 23:13:37 +0530

    render backend url updated.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./Pages/Signup.jsx";
-import Login from "./Pages/login.jsx";
+import Login from "./Pages/Login.jsx";
 import ProtectedRoute from "./configs/ProtectedRoute.jsx";
 import Dashboard from "./Pages/Dashboard.jsx";
 import Navbar from "./components/Navbar.jsx";
--- Frontend/src/configs/axiosInstance.js
+++ Frontend/src/configs/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://auth-system-basic-1.onrender.com",
   timeout: 10000,
   withCredentials: true,
   headers: {
